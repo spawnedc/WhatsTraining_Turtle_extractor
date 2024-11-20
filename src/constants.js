eb6dc6409ea0eeb4e21b7e5bb24dedc1c6758ffb
Can Yilmaz <can@yilmaz.app>
2024-11-20 16:24:37 +0000

    lint

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const process = require("process");
+const path = require("path")
+const process = require("process")
 
-const BASE_DIR = process.cwd();
-const SRC_DIR = "src";
-const BUILD_DIR = "build";
-const EXPORT_DIR = "exports";
-const CLASSES_DIR = path.join(EXPORT_DIR, "classes");
-const LUA_DIR = path.join(BASE_DIR, BUILD_DIR, "lua");
+const BASE_DIR = process.cwd()
+const SRC_DIR = "src"
+const BUILD_DIR = "build"
+const EXPORT_DIR = "exports"
+const CLASSES_DIR = path.join(EXPORT_DIR, "classes")
+const LUA_DIR = path.join(BASE_DIR, BUILD_DIR, "lua")
 
 const SPELL_ATTRIBUTES = {
   SPELL_ATTR0_DO_NOT_DISPLAY_SPELLBOOK_AURA_ICON_COMBAT_LOG: 8,
   SPELL_ATTR0_DO_NOT_LOG: 9,
-};
+}
 
 /**
  * For some reason these are not excluded by any filters and I couldn't find a
@@ -26,7 +26,7 @@ const SERVER_HIDDEN_SPELLS = [
   27803, // Priest: Holy Nova Rank 4
   27804, // Priest: Holy Nova Rank 5
   27805, // Priest: Holy Nova Rank 6
-];
+]
 
 module.exports = {
   BASE_DIR,
@@ -37,4 +37,4 @@ module.exports = {
   LUA_DIR,
   SPELL_ATTRIBUTES,
   SERVER_HIDDEN_SPELLS,
-};
+}
--- src/export.js
+++ src/export.js
@@ -1,7 +1,7 @@
-const { execSync } = require("child_process");
-const { writeFileSync } = require("fs");
-const path = require("path");
-const getIdsFromMask = require("./utils/getIdsFromMask");
+const { execSync } = require("child_process")
+const { writeFileSync } = require("fs")
+const path = require("path")
+const getIdsFromMask = require("./utils/getIdsFromMask")
 const {
   BASE_DIR,
   SRC_DIR,
@@ -9,92 +9,92 @@ const {
   CLASSES_DIR,
   SPELL_ATTRIBUTES,
   SERVER_HIDDEN_SPELLS,
-} = require("./constants");
+} = require("./constants")
 
 const hiddenSpellAttributes = [
   SPELL_ATTRIBUTES.SPELL_ATTR0_DO_NOT_DISPLAY_SPELLBOOK_AURA_ICON_COMBAT_LOG,
   // SPELL_ATTRIBUTES.SPELL_ATTR0_DO_NOT_LOG,
-];
+]
 
-const EXTRACT_DBC = false;
+const EXTRACT_DBC = process.argv[2] === "true"
 
-execSync(`mkdir -p ${path.join(BASE_DIR, SRC_DIR, CLASSES_DIR)}`);
+execSync(`mkdir -p ${path.join(BASE_DIR, SRC_DIR, CLASSES_DIR)}`)
 
 const arrayToObject = (array, keyField) =>
   array.reduce((obj, item) => {
-    obj[item[keyField]] = item;
-    return obj;
-  }, {});
+    obj[item[keyField]] = item
+    return obj
+  }, {})
 
 const extractFile = (dbName) => {
-  const moduleFile = `${path.join(EXPORT_DIR, dbName)}.json`;
+  const moduleFile = `${path.join(EXPORT_DIR, dbName)}.json`
   if (EXTRACT_DBC) {
-    console.info("Extracting", dbName, "as", moduleFile, "...");
+    console.info("Extracting", dbName, "as", moduleFile, "...")
     execSync(
       `npm run start -- ${dbName} --file=${path.join(
         BASE_DIR,
         SRC_DIR,
         moduleFile
       )}`
-    );
+    )
   }
-  return `./${moduleFile}`;
-};
+  return `./${moduleFile}`
+}
 
-const FILE_SKILL_RACE_CLASS_INFO = extractFile("SkillRaceClassInfo");
-const FILE_CLASSES = extractFile("ChrClasses");
-const FILE_RACES = extractFile("ChrRaces");
-const FILE_ALL_SPELLS = extractFile("Spell");
-const FILE_SKILL_LINES = extractFile("SkillLine");
-const FILE_SKILL_LINE_ABILITIES = extractFile("SkillLineAbility");
-const FILE_SPELL_ICONS = extractFile("SpellIcon");
-const FILE_TALENT_TABS = extractFile("TalentTab");
-const FILE_TALENTS = extractFile("Talent");
+const FILE_SKILL_RACE_CLASS_INFO = extractFile("SkillRaceClassInfo")
+const FILE_CLASSES = extractFile("ChrClasses")
+const FILE_RACES = extractFile("ChrRaces")
+const FILE_ALL_SPELLS = extractFile("Spell")
+const FILE_SKILL_LINES = extractFile("SkillLine")
+const FILE_SKILL_LINE_ABILITIES = extractFile("SkillLineAbility")
+const FILE_SPELL_ICONS = extractFile("SpellIcon")
+const FILE_TALENT_TABS = extractFile("TalentTab")
+const FILE_TALENTS = extractFile("Talent")
 
-const skillRaceClassInfo = require(FILE_SKILL_RACE_CLASS_INFO);
-const talentTabs = require(FILE_TALENT_TABS);
-const talents = require(FILE_TALENTS);
-const classes = require(FILE_CLASSES);
-const races = require(FILE_RACES);
-const racesById = arrayToObject(races, "ID");
+const skillRaceClassInfo = require(FILE_SKILL_RACE_CLASS_INFO)
+const talentTabs = require(FILE_TALENT_TABS)
+const talents = require(FILE_TALENTS)
+const classes = require(FILE_CLASSES)
+const races = require(FILE_RACES)
+const racesById = arrayToObject(races, "ID")
 
 const allClasses = classes.map((cls) => ({
   name: cls.Name_Lang_enUS,
   fileName: cls.Filename,
   classSet: cls.SpellClassSet,
   classMask: Math.pow(2, cls.ID - 1),
-}));
+}))
 
-const allSpells = require(FILE_ALL_SPELLS);
-const allSpellsById = arrayToObject(allSpells, "ID");
+const allSpells = require(FILE_ALL_SPELLS)
+const allSpellsById = arrayToObject(allSpells, "ID")
 
-const spellIcons = require(FILE_SPELL_ICONS);
-const spellIconsById = arrayToObject(spellIcons, "ID");
+const spellIcons = require(FILE_SPELL_ICONS)
+const spellIconsById = arrayToObject(spellIcons, "ID")
 
-const skillLines = require(FILE_SKILL_LINES);
-const skillLinesById = arrayToObject(skillLines, "ID");
-const skillLineAbilities = require(FILE_SKILL_LINE_ABILITIES);
-const skillLineAbilitiesBySpellId = arrayToObject(skillLineAbilities, "Spell");
+const skillLines = require(FILE_SKILL_LINES)
+const skillLinesById = arrayToObject(skillLines, "ID")
+const skillLineAbilities = require(FILE_SKILL_LINE_ABILITIES)
+const skillLineAbilitiesBySpellId = arrayToObject(skillLineAbilities, "Spell")
 
 const getSpellRanks = (spells, spellName, nameField, subTextField) =>
   spells
     .filter((spl) => spl[nameField] === spellName)
     .filter((spl) => !!spl[subTextField])
-    .sort((a, b) => a[subTextField].localeCompare(b[subTextField]));
+    .sort((a, b) => a[subTextField].localeCompare(b[subTextField]))
 
 const getRequiredIdForSpellId = (spellId, ranks) => {
   if (ranks.length <= 1) {
-    return undefined;
+    return undefined
   }
 
-  const spellRankIndex = ranks.findIndex((r) => r.id === spellId);
+  const spellRankIndex = ranks.findIndex((r) => r.id === spellId)
 
   if (spellRankIndex <= 0) {
-    return undefined;
+    return undefined
   }
 
-  return ranks[spellRankIndex - 1].id;
-};
+  return ranks[spellRankIndex - 1].id
+}
 
 const getSpellOverrides = (spell) => {
   const spellsWithSameName = getSpellRanks(
@@ -102,38 +102,37 @@ const getSpellOverrides = (spell) => {
     spell.name,
     "Name_Lang_enUS",
     "NameSubtext_Lang_enUS"
-  );
+  )
 
-  const spellIds = spellsWithSameName.map((s) => s.ID);
+  const spellIds = spellsWithSameName.map((s) => s.ID)
   const slas = spellIds
     .map((spellId, index) => {
-      const sla = skillLineAbilitiesBySpellId[spellId];
+      const sla = skillLineAbilitiesBySpellId[spellId]
       if (index === 0 && sla?.SupercededBySpell) {
-        return [spellId, sla.SupercededBySpell];
+        return [spellId, sla.SupercededBySpell]
       }
-      return sla?.SupercededBySpell;
+      return sla?.SupercededBySpell
     })
-    .flat();
+    .flat()
 
-  const filteredSlaIds = slas.filter((sla) => sla);
+  const filteredSlaIds = slas.filter((sla) => sla)
 
-  return filteredSlaIds;
-};
+  return filteredSlaIds
+}
 
 allClasses.forEach((cls) => {
-  console.info(`Exporting ${cls.fileName} spells...`);
+  console.info(`Exporting ${cls.fileName} spells...`)
 
   const classSkillLines = skillRaceClassInfo
     .filter((srci) => srci.ClassMask === cls.classMask)
     // this...
-    .filter(srci => !getIdsFromMask(srci.Flags).includes(2));
+    .filter((srci) => !getIdsFromMask(srci.Flags).includes(2))
+
   const classSkillLinesIds = classSkillLines
     .map((csl) => csl.SkillID)
     .filter(
       (skillId) => skillLinesById[skillId]?.CategoryID === 7 // Class Skills only
-    );
-
-  console.info(classSkillLinesIds)
+    )
 
   const classSkillLineAbilities = skillLineAbilities
     // Only include class skill lines
@@ -142,41 +141,41 @@ allClasses.forEach((cls) => {
     // Hopefully this will exclude spells like Clearcasting
     .filter((sla) => sla.ClassMask === cls.classMask)
     // Exclude spells hidden by the server
-    .filter((sla) => !SERVER_HIDDEN_SPELLS.includes(sla.Spell));
+    .filter((sla) => !SERVER_HIDDEN_SPELLS.includes(sla.Spell))
 
-  const classTalentTabsById = arrayToObject(talentTabs, "ID");
+  const classTalentTabsById = arrayToObject(talentTabs, "ID")
 
   const classTalentTabIds = talentTabs
     .filter((tt) => tt.ClassMask === cls.classMask)
-    .map((tt) => tt.ID);
+    .map((tt) => tt.ID)
   const classTalents = talents.filter((t) =>
     classTalentTabIds.includes(t.TabID)
-  );
+  )
   const classTalentSpells = classTalents
     .filter((t) => t.Flags === 1)
     .map((t) => ({
       id: t.ID,
       spell: t.SpellRank_1,
-      level: 10 + (t.TierID * 5),
+      level: 10 + t.TierID * 5,
       // Lua indexes start from 1 :sigh:
       tabIndex: classTalentTabsById[t.TabID].OrderIndex + 1,
-    }));
+    }))
 
-  let hasRaceMask = false;
+  let hasRaceMask = false
 
-  const overriddenSpellsMap = {};
+  const overriddenSpellsMap = {}
 
   const allClassSpells = classSkillLineAbilities
     .map((sla) => {
-      const spell = allSpellsById[sla.Spell];
+      const spell = allSpellsById[sla.Spell]
       const spellAttributes =
-        spell.Attributes !== 0 ? getIdsFromMask(spell.Attributes) : [];
+        spell.Attributes !== 0 ? getIdsFromMask(spell.Attributes) : []
       if (
         hiddenSpellAttributes.some((hiddenSpellAttribute) =>
           spellAttributes.includes(hiddenSpellAttribute)
         )
       ) {
-        return undefined;
+        return undefined
       }
 
       const races =
@@ -185,12 +184,12 @@ allClasses.forEach((cls) => {
               // UnitRace("player") returns the name, not the ID
               (raceId) => racesById[raceId].Name_Lang_enUS
             )
-          : undefined;
+          : undefined
       const isTaughtByTalent = classTalentSpells.find(
         (cts) => cts.spell === spell.ID
-      );
+      )
 
-      hasRaceMask = hasRaceMask || !!races;
+      hasRaceMask = hasRaceMask || !!races
 
       const newSpell = {
         id: spell.ID,
@@ -202,19 +201,19 @@ allClasses.forEach((cls) => {
         requiredTalent: isTaughtByTalent
           ? { id: isTaughtByTalent.id, tabIndex: isTaughtByTalent.tabIndex }
           : undefined,
-      };
+      }
 
       if (sla.SupercededBySpell) {
-        const overrideIds = getSpellOverrides(newSpell);
+        const overrideIds = getSpellOverrides(newSpell)
         if (overrideIds.length > 1) {
-          overriddenSpellsMap[overrideIds[0]] = overrideIds;
+          overriddenSpellsMap[overrideIds[0]] = overrideIds
         }
       }
 
-      return newSpell;
+      return newSpell
     })
     .filter((spell) => spell)
-    .filter((spell) => spell.level > 0);
+    .filter((spell) => spell.level > 0)
 
   const classSpells = allClassSpells.map((spell) => {
     const ranks = getSpellRanks(allClassSpells, spell.name, "name", "subText");
@@ -230,17 +229,17 @@ allClasses.forEach((cls) => {
     return spell;
   });
 
-  const spellsByLevel = Object.groupBy(classSpells, ({ level }) => level);
+  const spellsByLevel = Object.groupBy(classSpells, ({ level }) => level)
 
   const classData = {
     fileName: cls.fileName,
     hasRaceMask,
     overriddenSpellsMap: Object.values(overriddenSpellsMap),
     spellsByLevel,
-  };
+  }
 
   writeFileSync(
     path.join(BASE_DIR, SRC_DIR, CLASSES_DIR, `${cls.fileName}.json`),
     JSON.stringify(classData, undefined, 2)
-  );
-});
+  )
+})
